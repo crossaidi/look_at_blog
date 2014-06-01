87e3bd4fcdb1ed080aca5fc1f26491575d61895d
Kovalevsky Dmitry <deecross33@gmail.com>
2014-06-01 21:30:56 +0400

    Model props order fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,16 @@
 class Post < ActiveRecord::Base
   default_scope -> { order(date: :desc) }
 
+  has_attached_file :image, styles: { thumb: "250x250#", medium: "300x300#" },\
+    path: ':rails_root/public/uploads/:attachment/:style/:filename'
+
   validates :name, presence: true, length: { minimum: 1, maximum: 250 }
   validates :body, presence: true
+
   validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/png"] }
   validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
 
   before_save :fill_additional_data
-
-  has_attached_file :image, styles: { thumb: "250x250#", medium: "300x300#" },\
-    path: ':rails_root/public/uploads/:attachment/:style/:filename'
 
   protected
     def fill_additional_data
