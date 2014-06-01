95f13cb5cb03001fa06407839e6d157a328de012
Kovalevsky Dmitry <deecross33@gmail.com>
2014-06-01 22:52:56 +0400

    Redirect route test removing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   end
   # resources :posts, path: '/api/posts'
 
-  get '*path' => redirect('/')
+  # get '*path' => redirect('/')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
