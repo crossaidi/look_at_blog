5300b08a05171134af2091c667bfa8ca760cda9b
Kovalevsky Dmitry <deecross33@gmail.com>
2014-06-01 23:37:20 +0400

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
     resources :posts
   end
   # resources :posts, path: '/api/posts'
-
-  # get '*path' => redirect('/')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'main#index'
 
+  get '*path' => redirect('/')
   # Example of regular route:
+
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
