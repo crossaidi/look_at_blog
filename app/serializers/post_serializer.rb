f25799325b5471ca9a6e705c7d3f1b3c75d55e73
Kovalevsky Dmitry <deecross33@gmail.com>
2014-06-02 09:28:01 +0400

    Add result field in serializator

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :name, :body, :excerpt, :date, :image_file_name
+  attributes :result, :id, :name, :body, :excerpt, :date, :image_file_name
+
   root false
+
+  def result
+    1
+  end
 end
